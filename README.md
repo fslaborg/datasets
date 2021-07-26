e1885c55d81b990fd3f97ad2fa2d6b873a8677f5
Kevin Schneider <schneider.kev@outlook.de>
2021-07-26 14:54:06 +0200

    Init docs

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # datasets
-A data source for example datasets for all kinds of data science
+A data source for example datasets for all kinds of data science.
+
+For now, this repository just aims to aggregate some data sources that can be referenced by HTTP requests in a data science project. In the future, this ideally evolves to a proper nuget package that provides the data as dataframes directly.
---
+++ docs/index.fsx
@@ -0,0 +1,22 @@
+(**
+
+# FsLab data sets
+A data source for example datasets for all kinds of data science.
+
+For now, this repository just aims to aggregate some data sources that can be referenced by HTTP requests in a data science project. In the future, this ideally evolves to a proper nuget package that provides the data as dataframes directly.
+For using the data contained in this repository, we recommend using `FSharp.Data` in conjunction with `Deedle`, like this:
+
+*)
+
+#r "nuget: FSharp.Data"
+#r "nuget: Deedle"
+
+open FSharp.Data
+open Deedle
+
+let rawData = Http.RequestString ""
+let df = Frame.ReadCsvString(rawData) //exact settings may differ here depending on e.g. the separator used in the individual dataset
+
+df.Print()
+
+(***include-output***)
